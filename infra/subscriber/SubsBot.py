6c98055ea751ff0717aa444b639ff4baa28f04ab
Jonathan Prado <jonathan.prado@intraway.com>
2021-04-02 02:34:23 -0300

    update all data

--- infra/subscriber/SubsBot.py
+++ infra/subscriber/SubsBot.py
@@ -1,32 +1,31 @@
 import paho.mqtt.client as mqtt
-from influxdb import InfluxDBClient # FOR write data in influxDB
+from influxdb import InfluxDBClient # FOR write data on influxDB
 import time
 import socket
 
 
 
 # constantes 
-broker_address      = "mosquitto"  #Broker address
-database_address    = "influxdb"
-port                = 1883                 #Broker port
+broker_address      = "mosquitto"    # Broker address
+database_address    = "influxdb"    # DB address
+port                = 1883              # Broker port
 influxport          = 8086
-user                = "admin"               #Connection username
-password            = "admin"           #Connection password
-topic               = "#"                #Subscribe to ALL topics
-db_name             = "IoT"
-client_name         = "Subscriber"
+user                = "admin"           # Connection username
+password            = "admin"           # Connection password
+topic               = "#"               # Subscribe to ALL topics
+db_name             = "IoT"             # DB name
+client_name         = "Subscriber"      
 intent              = 0
 
 
 def on_message(client, userdata, message):
-    print("received message: " ,str(message.payload.decode("utf-8")))
+    #print("received message: " ,str(message.payload.decode("utf-8")))
     value       = str(message.payload.decode("utf-8"))   
     xvalue      = float(value)
-    
-    #dbClient    = InfluxDBClient(database_address, influxport, user, password, db_name)
-    #loginEvents = [{"measurement":"IoT", "fields":{ message.topic : xvalue } }]
-    #writedata   = dbClient.write_points(loginEvents)
-
+    print(message.topic , str(value))
+    dbClient    = InfluxDBClient(database_address, influxport, user, password, db_name)
+    loginEvents = [{"measurement":message.topic, "fields":{ message.topic : xvalue } }]
+    writedata   = dbClient.write_points(loginEvents)
 
 def on_subscribe():
     client      = mqtt.Client(client_name)
@@ -37,7 +36,6 @@ def on_subscribe():
     time.sleep(5)
     client.loop_stop()
 
-
 def DoMagic():
     a_socket        = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     location        = (broker_address, port)
@@ -47,7 +45,6 @@ def DoMagic():
     else:
        print("Port is not responding, wait a few seconds...")
        time.sleep(15)
-
 
 
 time.sleep(10) # wait for docker compose up 
